9587b82509eccaa5f5b9f46ececa972b7a143803
Joeri Timmermans <joeri.timmermans@intracto.com>
2016-12-08 11:41:00 +0100

    Fixed dependency injection

--- DependencyInjection/LexikTranslationSonataExtension.php
+++ DependencyInjection/LexikTranslationSonataExtension.php
@@ -5,6 +5,7 @@ namespace LexikTranslationSonataBundle\DependencyInjection;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\Config\FileLocator;
 
 class LexikTranslationSonataExtension extends Extension
 {
@@ -18,5 +19,8 @@ class LexikTranslationSonataExtension extends Extension
 
         $container->setParameter('lexik_translation_sonata.auto_discover', $config['auto_discover']);
         $container->setParameter('lexik_translation_sonata.auto_discover_domains', $config['auto_discover_domains']);
+
+        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.xml');
     }
 }
